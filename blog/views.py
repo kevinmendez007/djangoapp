c98d70112e65ff6359e568650ca07ed2e74014d7
Kevin Mendez <kevinmendez1007@gmail.com>
2016-09-30 08:36:29 -0700

    Se agrego un articulo y cada vez que se le da al titulo, lo redirige a una pagina a ese articulo que se le da click

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Postear
 
 # Create your views here.
 def listar_articulo(request):
     articulo = Postear.objects.all()
     return render(request, 'blog/listar_articulo.html', {'articulo':articulo})
+
+def detalle_articulo(request, pk):
+        post = get_object_or_404(Postear, pk=pk)
+        return render(request, 'blog/detalle_articulo.html', {'post': post})
